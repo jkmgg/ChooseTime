b2fd86b99a880f9f12b178127576a4bb285aa420
gyl <chinagjkm@gmail.com>
2017-04-06 10:39:39 +0800

    Initial Commit

---
+++ shijian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shijian
+//
+//  Created by IOS_MAC PRO on 2017/4/6.
+//  Copyright © 2017年 com.ASS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
